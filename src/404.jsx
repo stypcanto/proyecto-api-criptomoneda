68fceb3ab648528ea9f68054019ff80706978ab6
Styp Canto <styp611@outlook.com>
2025-02-17 00:08:22 -0500

    Creando un React Router DOM

---
+++ src/404.jsx
@@ -0,0 +1,10 @@
+const _404 = () => {
+  return (
+    <>
+      <h1>404 - Not Found</h1>
+      <span> Página no existe </span>
+    </>
+  );
+};
+
+export default _404;
--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,18 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
+import Pagina404 from './404'
 import "./main.css"
+import Saludo from './saludo'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-    <App />
+   <BrowserRouter>
+        <Routes>
+            <Route path='/' element={<App />} />
+            <Route path='/saludo' element={<Saludo />} />
+            <Route path='*' element={<Pagina404 />} />
+        </Routes>
+   </BrowserRouter>
+    
 )
