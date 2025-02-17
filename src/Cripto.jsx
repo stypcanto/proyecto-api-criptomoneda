127d641575c47fad3b2e74e449949b51c0826e0d
Styp Canto <styp611@outlook.com>
2025-02-16 22:54:42 -0500

    Dandole estilo a mi pagina de cripto, se creo archivcs y estilos para manejar dinamiacamente, se puede diferencias en positivo y negativo la variacion en las 24 horas

---
+++ src/Cripto.jsx
@@ -0,0 +1,24 @@
+import "./Cripto.css"
+
+const Cripto = ({name, priceUsd, symbol,changePercent24Hr}) => {
+    return (
+        <div className="cripto">
+           <h2> {name} </h2>
+           <div className="info"> 
+                <p> <span className="label">Precio: </span> {parseFloat(priceUsd).toFixed(4)}</p>
+                     {/* Aquí se convierte a número y se limita a 4 decimales */} 
+                     <p> <span className="label">Código: </span> {symbol} </p>
+                     <p> 
+                        <span className="label">Variación 24hrs: </span>
+                        <span className={`${parseFloat(changePercent24Hr) > 0 ? "positivo" : "negativo"}`}>
+                            {parseFloat(changePercent24Hr).toFixed(3)}%
+                        </span> 
+                         
+                    </p>
+           </div>
+          
+        </div>  
+    )
+}
+
+export default Cripto;
